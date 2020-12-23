f529aef691691bab2223b0306c26d124e3e940f5
George Mironov <yegor.mironovwill@gmail.com>
2020-12-23 18:47:01 +0300

    One fixture session and Chrome instead of Firefox

--- fixture/application.py
+++ fixture/application.py
@@ -1,10 +1,10 @@
-from selenium.webdriver import Firefox
+from selenium.webdriver import Chrome
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 
 class Application:
     def __init__(self):
-        self.driver = Firefox()
+        self.driver = Chrome()
         self.driver.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
