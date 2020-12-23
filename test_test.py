d64e765bab192ea1ede4ca5f8522828f2b45fd42
George Mironov <yegor.mironovwill@gmail.com>
2020-12-23 11:45:18 +0300

    Extract parameters in login and create group methods

--- test_test.py
+++ test_test.py
@@ -6,42 +6,42 @@ driver.implicitly_wait(5)
 def open_home_page():
     driver.get("http://localhost/addressbook/")
 
-def login():
+def login(username, password):
     #username
     username_field = driver.find_element_by_name("user")
     username_field.clear()
-    username_field.send_keys("admin")
+    username_field.send_keys(username)
 
     #password
     pass_field = driver.find_element_by_name("pass")
     pass_field.click()
     pass_field.clear()
-    pass_field.send_keys("secret")
+    pass_field.send_keys(password)
     driver.find_element_by_css_selector("input[value=Login]").click()
 
 def open_groups_page():
     driver.find_element_by_css_selector("#nav a[href='group.php']").click()
 
-def create_group():
+def create_group(name, header, footer):
     driver.find_element_by_name("new").click()
 
     #name
     group_name = driver.find_element_by_name("group_name")
     group_name.click()
     group_name.clear()
-    group_name.send_keys("Friends")
+    group_name.send_keys(name)
 
     #header
     group_header = driver.find_element_by_name("group_header")
     group_header.click()
     group_header.clear()
+    group_header.send_keys(header)
 
     #footer
-    group_header.send_keys("Mine")
     group_footer = driver.find_element_by_name("group_footer")
     group_footer.click()
     group_footer.clear()
-    group_footer.send_keys("Dear ones")
+    group_footer.send_keys(footer)
     driver.find_element_by_css_selector("#content [name=submit]").click()
 
 def return_to_groups_page():
@@ -52,9 +52,9 @@ def logout():
 
 
 open_home_page()
-login()
+login(username="admin", password="secret")
 open_groups_page()
-create_group()
+create_group("Friends", "Mine", "Dear ones")
 return_to_groups_page()
 logout()
 
