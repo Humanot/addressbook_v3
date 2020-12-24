d207e289b4feb1538cbe27b735e2a9c393a1b897
George Mironov <yegor.mironovwill@gmail.com>
2020-12-24 21:25:06 +0300

    Provide preconditions for delete & modify group tests along with groups count method

--- fixture/group.py
+++ fixture/group.py
@@ -49,6 +49,11 @@ class GroupHelper:
         driver.find_element_by_name("update").click()
         self.return_to_groups_page()
 
+    def count(self):
+        driver = self.app.driver
+        self.open_groups_page()
+        return len(driver.find_elements_by_name("selected[]"))
+
     def return_to_groups_page(self):
         driver = self.app.driver
         driver.find_element_by_css_selector(".msgbox [href='group.php']").click()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,5 +1,7 @@
 from model.group import Group
 
 def test_modify_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="Friends", header="Mine", footer="Dear ones"))
     app.group.modify_first(Group(name="Relations"))
 
