8e7d184b7ac94e8ceb25c2c32ffdd5edb6375be6
George Mironov <yegor.mironovwill@gmail.com>
2020-12-24 18:39:45 +0300

    General login and logout

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,5 @@
 from model.group import Group
 
 def test_create_new_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="Friends", header="Mine", footer="Dear ones"))
-    app.session.logout()
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,6 +1,5 @@
 from model.group import Group
 
 def test_modify_first_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first(Group(name="Relations"))
-    app.session.logout()
+
