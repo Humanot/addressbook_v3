997233f0492dda1aacae984cf00cd0beb2ba0d25
George Mironov <yegor.mironovwill@gmail.com>
2021-01-05 20:07:49 +0300

    Deeper comparison of groups list in modify test group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,8 +3,12 @@ from model.group import Group
 def test_modify_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Friends", header="Mine", footer="Dear ones"))
+    group = Group(name="Relations")
     old_groups = app.group.get_list()
-    app.group.modify_first(Group(name="Relations"))
+    group.id = old_groups[0].id #save id of changing group
+    app.group.modify_first(group)
     new_groups = app.group.get_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group #name + id
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
