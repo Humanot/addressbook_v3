6e0691c68a1ae2818b7e50825c8b4759265ba775
George Mironov <yegor.mironovwill@gmail.com>
2021-01-07 20:18:45 +0300

    Change list box value and upload photo methods in ContactHelper

--- fixture/contact.py
+++ fixture/contact.py
@@ -8,6 +8,7 @@ class ContactHelper:
         driver = self.app.driver
         driver.find_element_by_css_selector("#nav a[href='edit.php']").click()
         self.fill_contact_info(contact_data)
+        driver.find_element_by_css_selector("form[name=theform] input[name=submit]").click()
         self.return_to_home_page()
 
     def return_to_home_page(self):
@@ -21,16 +22,23 @@ class ContactHelper:
         name_field.clear()
         name_field.send_keys(data_field)
 
-    def fill_contact_info(self, contact_data):
+    def upload_photo(self, data_field, field_name):
         driver = self.app.driver
+        photo_field = driver.find_element_by_name(field_name)
+        photo_field.send_keys(data_field)
+
+    def change_listbox_value(self, box_name, value):
+        driver = self.app.driver
+        select_list = Select(driver.find_element_by_name(box_name))
+        select_list.select_by_visible_text(value)
+
+    def fill_contact_info(self, contact_data):
         self.change_field_value(contact_data.firstname, "firstname")
         self.change_field_value("Dunno", "middlename")
         self.change_field_value(contact_data.lastname, "lastname")
         self.change_field_value("Viking", "nickname")
 
-        #photo
-        photo_field = driver.find_element_by_name("photo")
-        photo_field.send_keys("D:/1. Pictures/Avatars/team av/231587506.jpg")
+        self.upload_photo("D:/1. Pictures/Avatars/team av/231587506.jpg", "photo")
 
         self.change_field_value("Title", "title")
         self.change_field_value("Coins", "company")
@@ -49,25 +57,21 @@ class ContactHelper:
         self.change_field_value("vk.com", "homepage")
 
         #Birthday
-        bday_select = Select(driver.find_element_by_name("bday"))
-        bday_select.select_by_visible_text("10")
-        bmonth_select = Select(driver.find_element_by_name("bmonth"))
-        bmonth_select.select_by_visible_text("May")
+        self.change_listbox_value("bday", "10")
+        self.change_listbox_value("bmonth", "May")
         self.change_field_value("1989", "byear")
 
         #Anniversary
-        aday_select = Select(driver.find_element_by_name("aday"))
-        aday_select.select_by_visible_text("14")
-        amonth_select = Select(driver.find_element_by_name("amonth"))
-        amonth_select.select_by_visible_text("March")
+        self.change_listbox_value("aday", "14")
+        self.change_listbox_value("amonth", "March")
         self.change_field_value("2000", "ayear")
 
-        group_select = Select(driver.find_element_by_name("new_group"))
-        group_select.select_by_visible_text("[none]")
+        #contact_group
+        self.change_listbox_value("new_group", "[none]")
 
         self.change_field_value("Theres nothing", "address2")
         self.change_field_value(contact_data.homephone2, "phone2")
         self.change_field_value("Origato", "notes")
 
-        driver.find_element_by_css_selector("form[name=theform] input[name=submit]").click()
+
 
