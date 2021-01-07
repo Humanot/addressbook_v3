23d309e32579194e55718f926b896b6da50d03ab
George Mironov <yegor.mironovwill@gmail.com>
2021-01-07 23:33:55 +0300

    Validating homepage and edit page phones by splitlines method test and fix some bugs in open view page by index and open edit page by index ContactHelper methods

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,8 +1,8 @@
 from selenium.webdriver.support.ui import Select
 from model.contact import Contact
 
-class ContactHelper:
 
+class ContactHelper:
     contacts_list_cache = None
 
     def __init__(self, app):
@@ -62,24 +62,24 @@ class ContactHelper:
         self.change_field_value(contact_data.workphone, "work")
         self.change_field_value(contact_data.fax, "fax")
 
-        #Emails:
+        # Emails:
         self.change_field_value(contact_data.email, "email")
         self.change_field_value(contact_data.email2, "email2")
         self.change_field_value(contact_data.email3, "email3")
 
         self.change_field_value(contact_data.homepage, "homepage")
 
-        #Birthday
+        # Birthday
         self.change_listbox_value("bday", contact_data.bday)
         self.change_listbox_value("bmonth", contact_data.bmonth)
         self.change_field_value(contact_data.byear, "byear")
 
-        #Anniversary
+        # Anniversary
         self.change_listbox_value("aday", contact_data.aday)
         self.change_listbox_value("amonth", contact_data.amonth)
         self.change_field_value(contact_data.ayear, "ayear")
 
-        #contact_group
+        # contact_group
         self.change_listbox_value("new_group", contact_data.group)
 
         self.change_field_value(contact_data.address2, "address2")
@@ -93,12 +93,15 @@ class ContactHelper:
             self.app.open_home_page()
             contacts_rows = driver.find_elements_by_css_selector("#maintable tr[name=entry]")
             for row in contacts_rows:
-                #cells = row.find_elements_by_tag_name("td)"
-                id = row.find_element_by_css_selector("td input").get_attribute("value")
-                firstname = row.find_elements_by_tag_name("td")[2].text
-                lastname = row.find_elements_by_tag_name("td")[1].text
+                cells = row.find_elements_by_tag_name("td")
+                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+                firstname = cells[2].text
+                lastname = cells[1].text
+                all_phones = cells[5].text.splitlines()
 
-                self.contacts_list_cache.append(Contact(id=id, firstname=firstname, lastname=lastname))
+                self.contacts_list_cache.append(
+                    Contact(id=id, firstname=firstname, lastname=lastname, homephone=all_phones[0],
+                            mobile=all_phones[1], workphone=all_phones[2], homephone2=all_phones[3]))
         return list(self.contacts_list_cache)
 
     def count(self):
@@ -110,11 +113,25 @@ class ContactHelper:
         self.app.open_home_page()
         contact_row = driver.find_elements_by_css_selector("#maintable tr[name=entry]")[index]
         cell = contact_row.find_elements_by_tag_name("td")[6]
-        cell.contact_row.find_elements_by_tag_name("a").click()
+        cell.find_element_by_tag_name("a").click()
 
     def open_edit_page_by_index(self, index):
         driver = self.app.driver
         self.app.open_home_page()
         contact_row = driver.find_elements_by_css_selector("#maintable tr[name=entry]")[index]
         cell = contact_row.find_elements_by_tag_name("td")[7]
-        cell.contact_row.find_elements_by_tag_name("a").click()
+        cell.find_element_by_tag_name("a").click()
+
+    def get_info_from_edit_page(self, index):
+        driver = self.app.driver
+        self.open_edit_page_by_index(index)
+        id = driver.find_element_by_name("id").get_attribute("value")
+        firstname = driver.find_element_by_name("firstname").get_attribute("value")
+        lastname = driver.find_element_by_name("lastname").get_attribute("value")
+        homephone = driver.find_element_by_name("home").get_attribute("value")
+        workphone = driver.find_element_by_name("work").get_attribute("value")
+        mobile = driver.find_element_by_name("mobile").get_attribute("value")
+        homephone2 = driver.find_element_by_name("phone2").get_attribute("value")
+
+        return Contact(id=id, firstname=firstname, lastname=lastname, homephone=homephone, mobile=mobile,
+                       workphone=workphone, homephone2=homephone2)
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,9 +2,9 @@ from model.contact import Contact
 
 def test_create_new_contact(app):
     old_contacts = app.contact.get_list()
-    contact = Contact(firstname="Kirs", lastname="Vasiliev", homephone="555-666-666", mobile="89325848383",
+    contact = Contact(firstname="Kirs", lastname="Vasiliev", homephone="555666666", mobile="89325848383",
                       workphone="123",
-                      homephone2="444-444-555")
+                      homephone2="444444555")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_list()
