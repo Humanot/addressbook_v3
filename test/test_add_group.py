72d5d1a49ad88d5721c9a6b9e701e792f998dea2
George Mironov <yegor.mironovwill@gmail.com>
2021-01-10 22:59:22 +0300

    Parametrize add group test by pytest

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,8 +1,12 @@
 from model.group import Group
+import pytest
 
-def test_create_new_group(app):
+test_data = [Group(name="Friends", header="Mine", footer="Dear ones"),
+             Group(name="", header="", footer="")]
+
+@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
+def test_create_new_group(app, group):
     old_groups = app.group.get_list()
-    group = Group(name="Friends", header="Mine", footer="Dear ones")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_list()
