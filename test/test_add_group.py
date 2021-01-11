fa09636153fc766e77fac0a9bed917f68d17314a
George Mironov <yegor.mironovwill@gmail.com>
2021-01-11 20:35:01 +0300

    Isolate test data for add group test in py file

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,25 +1,6 @@
-from model.group import Group
 import pytest
-from random import choice, randrange
-from string import ascii_letters, digits, punctuation
-
-
-def random_string(prefix, maxlen):
-    symbols = ascii_letters + digits + punctuation + " " * 10
-    return prefix + "".join([choice(symbols) for i in range(randrange(maxlen))])
-
-
-test_data = [Group(name="Friends", header="Mine", footer="Dear ones"),
-             Group(name="", header="", footer="")] + [
-                Group(name=random_string("name", 10), header=random_string("header", 20),
-                      footer=random_string("footer", 20)) for i in range(5)]
-
-# test_data = [
-#     Group(name=name, header=header, footer=footer)
-#     for name in ["", random_string("name", 10)]
-#     for header in ["", random_string("header", 20)]
-#     for footer in ["", random_string("footer", 20)]
-# ]
+from model.group import Group
+from data.add_group import constant as test_data
 
 @pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
 def test_create_new_group(app, group):
