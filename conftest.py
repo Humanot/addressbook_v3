bd39330a3f1ce9a510d6355f51f305b2e2d67a99
George Mironov <yegor.mironovwill@gmail.com>
2021-01-11 19:56:03 +0300

    Load json for base url and admin credentials

--- conftest.py
+++ conftest.py
@@ -1,17 +1,23 @@
 from fixture.application import Application
 import pytest
+from json import load
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
 
     if (fixture is None) or (not fixture.is_valid):
         browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--baseUrl")
-        fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+        #base_url = request.config.getoption("--baseUrl")
+        if target is None:
+            with open(request.config.getoption("--target")) as config_file:
+                target = load(config_file)
+        fixture = Application(browser=browser, base_url=target["BaseUrl"])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 @pytest.fixture(scope = "session", autouse=True)
@@ -24,3 +30,4 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
