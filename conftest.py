ecedb2f90a06e31e9d75cb76c1b50ad9b9040ac1
George Mironov <yegor.mironovwill@gmail.com>
2021-01-11 20:07:03 +0300

    Calculate cwd for jsonfile in conftest

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 from fixture.application import Application
 import pytest
 from json import load
+from os.path import abspath, dirname, join
 
 fixture = None
 target = None
@@ -13,8 +14,9 @@ def app(request):
     if (fixture is None) or (not fixture.is_valid):
         browser = request.config.getoption("--browser")
         #base_url = request.config.getoption("--baseUrl")
+        target_file = join(dirname(abspath(__file__)), request.config.getoption("--target"))
         if target is None:
-            with open(request.config.getoption("--target")) as config_file:
+            with open(target_file) as config_file:
                 target = load(config_file)
         fixture = Application(browser=browser, base_url=target["BaseUrl"])
     fixture.session.ensure_login(username=target["username"], password=target["password"])
@@ -31,3 +33,4 @@ def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
     parser.addoption("--target", action="store", default="target.json")
+
