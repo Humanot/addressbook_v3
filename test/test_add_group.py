ecc4949466bd586f501c281cf27c773d70b8fc56
George Mironov <yegor.mironovwill@gmail.com>
2021-02-25 18:20:05 +0300

    Describe Allure steps in main add group test

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,4 +1,5 @@
 # import pytest
+import allure
 from model.group import Group
 # from data.groups import constant as test_data
 
@@ -6,11 +7,14 @@ from model.group import Group
 # def test_create_new_group(app, data_groups):
 def test_create_new_group(app, db, json_groups):
     group = json_groups
-    #old_groups = app.group.get_list()
-    old_groups = db.get_list()
-    app.group.create(group)
-    #assert len(old_groups) + 1 == app.group.count() #old hashing through ui len
-    #new_groups = app.group.get_list()
-    new_groups = db.get_list()
-    old_groups.append(group)  # insert
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    with allure.step('Given a group list'):
+        #old_groups = app.group.get_list()
+        old_groups = db.get_list()
+    with allure.step(f'When I add the group {group} to the list'):
+        app.group.create(group)
+    with allure.step('Then the new group list is equal to the old list with the added group'):
+        #assert len(old_groups) + 1 == app.group.count() #old hashing through ui len
+        #new_groups = app.group.get_list()
+        new_groups = db.get_list()
+        old_groups.append(group)  # insert
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
